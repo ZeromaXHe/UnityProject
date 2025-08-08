42b02e6e69869640b2395123988e8415116ad3f6
Zhu XH <zhuxiaohe95@163.com>
2025-08-08 10:26:02 +0800

    feature(CatlikeCoding): 实现 2.2 Basics - 7 Organic Variety

--- URP 3D Test Project/Assets/CatlikeCodings/Basics/Fractals/FractalGpuJob.hlsl
+++ URP 3D Test Project/Assets/CatlikeCodings/Basics/Fractals/FractalGpuJob.hlsl
@@ -2,7 +2,8 @@
 StructuredBuffer<float3x4> _Matrices;
 #endif
 
-void ConfigureProcedural () {
+void ConfigureProcedural()
+{
     #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
     float3x4 m = _Matrices[unity_InstanceID];
     unity_ObjectToWorld._m00_m01_m02_m03 = m._m00_m01_m02_m03;
@@ -12,10 +13,35 @@ void ConfigureProcedural () {
     #endif
 }
 
-void ShaderGraphFunction_float (float3 In, out float3 Out) {
-    Out = In;
+float4 _ColorA, _ColorB;
+float4 _SequenceNumbers;
+
+float4 GetFractalColor()
+{
+    #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+    float4 color;
+    color.rgb = lerp(
+        _ColorA.rgb, _ColorB.rgb,
+        frac(unity_InstanceID * _SequenceNumbers.x + _SequenceNumbers.y)
+    );
+    color.a = lerp(
+        _ColorA.a, _ColorB.a,
+        frac(unity_InstanceID * _SequenceNumbers.z + _SequenceNumbers.w)
+    );
+    return color;
+    #else
+    return _ColorA;
+    #endif
 }
 
-void ShaderGraphFunction_half (half3 In, out half3 Out) {
+void ShaderGraphFunction_float(float3 In, out float3 Out, out float4 FractalColor)
+{
     Out = In;
+    FractalColor = GetFractalColor();
+}
+
+void ShaderGraphFunction_half(half3 In, out half3 Out, out half4 FractalColor)
+{
+    Out = In;
+    FractalColor = GetFractalColor();
 }
--- URP 3D Test Project/Assets/CatlikeCodings/Basics/Fractals/FractalJob.cs
+++ URP 3D Test Project/Assets/CatlikeCodings/Basics/Fractals/FractalJob.cs
@@ -5,19 +5,20 @@ using Unity.Mathematics;
 using UnityEngine;
 using static Unity.Mathematics.math;
 using quaternion = Unity.Mathematics.quaternion;
+using Random = UnityEngine.Random;
 
 namespace CatlikeCodings.Basics.Fractals
 {
     public class FractalJob : MonoBehaviour
     {
-        [SerializeField, Range(1, 8)] private int depth = 4;
-        [SerializeField] private Mesh mesh;
+        [SerializeField, Range(3, 8)] private int depth = 4;
+        [SerializeField] private Mesh mesh, leafMesh;
         [SerializeField] private Material material;
-
-        private static readonly float3[] Directions =
-        {
-            up(), right(), left(), forward(), back()
-        };
+        [SerializeField] private Gradient gradientA, gradientB;
+        [SerializeField] private Color leafColorA, leafColorB;
+        [SerializeField, Range(0f, 90f)] private float maxSagAngleA = 15f, maxSagAngleB = 25f;
+        [SerializeField, Range(0f, 90f)] private float spinSpeedA = 20f, spinSpeedB = 25f;
+        [SerializeField, Range(0f, 1f)] private float reverseSpinChance = 0.25f;
 
         private static readonly Quaternion[] Rotations =
         {
@@ -28,8 +29,8 @@ namespace CatlikeCodings.Basics.Fractals
         [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
         private struct UpdateFractalLevelJob : IJobFor
         {
-            public float SpinAngleDelta;
             public float Scale;
+            public float DeltaTime;
             [ReadOnly] public NativeArray<FractalPart> Parents;
             public NativeArray<FractalPart> Parts;
             [WriteOnly] public NativeArray<float3x4> Matrices;
@@ -38,10 +39,25 @@ namespace CatlikeCodings.Basics.Fractals
             {
                 var parent = Parents[i / 5];
                 var part = Parts[i];
-                part.SpinAngle += SpinAngleDelta;
-                part.WorldRotation = mul(parent.WorldRotation, mul(part.Rotation, quaternion.RotateY(part.SpinAngle)));
+                part.SpinAngle += part.SpinVelocity * DeltaTime;
+                var upAxis = mul(mul(parent.WorldRotation, part.Rotation), up());
+                var sagAxis = cross(up(), upAxis);
+                var sagMagnitude = length(sagAxis);
+                quaternion baseRotation;
+                if (sagMagnitude > 0f)
+                {
+                    sagAxis /= sagMagnitude;
+                    var sagRotation = quaternion.AxisAngle(sagAxis, part.MaxSagAngle * sagMagnitude);
+                    baseRotation = mul(sagRotation, parent.WorldRotation);
+                }
+                else
+                {
+                    baseRotation = parent.WorldRotation;
+                }
+
+                part.WorldRotation = mul(baseRotation, mul(part.Rotation, quaternion.RotateY(part.SpinAngle)));
                 part.WorldPosition =
-                    parent.WorldPosition + (Vector3)mul(parent.WorldRotation, 1.5f * Scale * part.Direction);
+                    parent.WorldPosition + (Vector3)mul(part.WorldRotation, float3(0f, 1.5f * Scale, 0f));
                 Parts[i] = part;
                 var r = float3x3(part.WorldRotation) * Scale;
                 Matrices[i] = float3x4(r.c0, r.c1, r.c2, part.WorldPosition);
@@ -50,23 +66,30 @@ namespace CatlikeCodings.Basics.Fractals
 
         private struct FractalPart
         {
-            public Vector3 Direction, WorldPosition;
+            public Vector3 WorldPosition;
             public Quaternion Rotation, WorldRotation;
-            public float SpinAngle;
+            public float MaxSagAngle, SpinAngle, SpinVelocity;
         }
 
-        private static FractalPart CreatePart(int childIndex) =>
+        private FractalPart CreatePart(int childIndex) =>
             new()
             {
-                Direction = Directions[childIndex],
+                MaxSagAngle = radians(Random.Range(maxSagAngleA, maxSagAngleB)),
                 Rotation = Rotations[childIndex],
+                SpinVelocity =
+                    (Random.value < reverseSpinChance ? -1f : 1f) * radians(Random.Range(spinSpeedA, spinSpeedB))
             };
 
-        private static readonly int MatricesId = Shader.PropertyToID("_Matrices");
+        private static readonly int ColorAId = Shader.PropertyToID("_ColorA"),
+            ColorBId = Shader.PropertyToID("_ColorB"),
+            MatricesId = Shader.PropertyToID("_Matrices"),
+            SequenceNumbersId = Shader.PropertyToID("_SequenceNumbers");
+
         private static MaterialPropertyBlock _propertyBlock;
         private NativeArray<FractalPart>[] _parts;
         private NativeArray<float3x4>[] _matrices;
         private ComputeBuffer[] _matricesBuffers;
+        private Vector4[] _sequenceNumbers;
 
 
         private void OnEnable()
@@ -74,12 +97,14 @@ namespace CatlikeCodings.Basics.Fractals
             _parts = new NativeArray<FractalPart>[depth];
             _matrices = new NativeArray<float3x4>[depth];
             _matricesBuffers = new ComputeBuffer[depth];
+            _sequenceNumbers = new Vector4[depth];
             const int stride = 12 * 4;
             for (int i = 0, length = 1; i < _parts.Length; i++, length *= 5)
             {
                 _parts[i] = new NativeArray<FractalPart>(length, Allocator.Persistent);
                 _matrices[i] = new NativeArray<float3x4>(length, Allocator.Persistent);
                 _matricesBuffers[i] = new ComputeBuffer(length, stride);
+                _sequenceNumbers[i] = new Vector4(Random.value, Random.value, Random.value, Random.value);
             }
 
             _parts[0][0] = CreatePart(0);
@@ -110,6 +135,7 @@ namespace CatlikeCodings.Basics.Fractals
             _parts = null;
             _matrices = null;
             _matricesBuffers = null;
+            _sequenceNumbers = null;
         }
 
         private void OnValidate()
@@ -129,9 +155,9 @@ namespace CatlikeCodings.Basics.Fractals
         // Update is called once per frame
         private void Update()
         {
-            var spinAngleDelta = 0.125f * PI * Time.deltaTime;
+            var deltaTime = Time.deltaTime;
             var rootPart = _parts[0][0];
-            rootPart.SpinAngle += spinAngleDelta;
+            rootPart.SpinAngle += rootPart.SpinVelocity * deltaTime;
             rootPart.WorldRotation = mul(transform.rotation,
                 mul(rootPart.Rotation, quaternion.RotateY(rootPart.SpinAngle)));
             rootPart.WorldPosition = transform.position;
@@ -146,7 +172,7 @@ namespace CatlikeCodings.Basics.Fractals
                 scale *= 0.5f;
                 jobHandle = new UpdateFractalLevelJob
                 {
-                    SpinAngleDelta = spinAngleDelta,
+                    DeltaTime = deltaTime,
                     Scale = scale,
                     Parents = _parts[li - 1],
                     Parts = _parts[li],
@@ -157,12 +183,32 @@ namespace CatlikeCodings.Basics.Fractals
             jobHandle.Complete();
 
             var bounds = new Bounds(rootPart.WorldPosition, 3f * objectScale * Vector3.one);
+            var leafIndex = _matricesBuffers.Length - 1;
             for (var i = 0; i < _matricesBuffers.Length; i++)
             {
                 var buffer = _matricesBuffers[i];
                 buffer.SetData(_matrices[i]);
+                Color colorA, colorB;
+                Mesh instanceMesh;
+                if (i == leafIndex)
+                {
+                    colorA = leafColorA;
+                    colorB = leafColorB;
+                    instanceMesh = leafMesh;
+                }
+                else
+                {
+                    var gradientInterpolator = i / (_matricesBuffers.Length - 2f);
+                    colorA = gradientA.Evaluate(gradientInterpolator);
+                    colorB = gradientB.Evaluate(gradientInterpolator);
+                    instanceMesh = mesh;
+                }
+
+                _propertyBlock.SetColor(ColorAId, colorA);
+                _propertyBlock.SetColor(ColorBId, colorB);
                 _propertyBlock.SetBuffer(MatricesId, buffer);
-                Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, buffer.count, _propertyBlock);
+                _propertyBlock.SetVector(SequenceNumbersId, _sequenceNumbers[i]);
+                Graphics.DrawMeshInstancedProcedural(instanceMesh, 0, material, bounds, buffer.count, _propertyBlock);
             }
         }
     }
