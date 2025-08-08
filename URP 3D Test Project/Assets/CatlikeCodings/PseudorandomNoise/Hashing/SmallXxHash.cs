5ffe5f5d630d935be19e2c2596580636571f285f
Zhu XH <zhuxiaohe95@163.com>
2025-08-08 11:47:00 +0800

    feature(CatlikeCoding): 实现 2.3 Pseudorandom Noise - 1 Hashing

---
+++ URP 3D Test Project/Assets/CatlikeCodings/PseudorandomNoise/Hashing/SmallXxHash.cs
@@ -0,0 +1,38 @@
+namespace CatlikeCodings.PseudorandomNoise.Hashing
+{
+    /// Copyright (C) 2025 Zhu Xiaohe(aka ZeromaXHe)
+    /// Author: Zhu XH (ZeromaXHe)
+    /// Date: 2025-08-08 11:02:08
+    public readonly struct SmallXxHash
+    {
+        private const uint PrimeA = 0b10011110001101110111100110110001;
+        private const uint PrimeB = 0b10000101111010111100101001110111;
+        private const uint PrimeC = 0b11000010101100101010111000111101;
+        private const uint PrimeD = 0b00100111110101001110101100101111;
+        private const uint PrimeE = 0b00010110010101100110011110110001;
+        private readonly uint _accumulator;
+
+        public SmallXxHash(uint accumulator)
+        {
+            _accumulator = accumulator;
+        }
+
+        public static implicit operator SmallXxHash(uint accumulator) => new(accumulator);
+
+        public static implicit operator uint(SmallXxHash hash)
+        {
+            var avalanche = hash._accumulator;
+            avalanche ^= avalanche >> 15;
+            avalanche *= PrimeB;
+            avalanche ^= avalanche >> 13;
+            avalanche *= PrimeC;
+            avalanche ^= avalanche >> 16;
+            return avalanche;
+        }
+
+        public static SmallXxHash Seed(int seed) => (uint)seed + PrimeE;
+        public SmallXxHash Eat(int data) => RotateLeft(_accumulator + (uint)data * PrimeC, 17) * PrimeD;
+        public SmallXxHash Eat(byte data) => RotateLeft(_accumulator + data * PrimeE, 11) * PrimeA;
+        private static uint RotateLeft(uint data, int steps) => (data << steps) | (data >> 32 - steps);
+    }
+}
